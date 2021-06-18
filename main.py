f40cd82414a1e7f208e7aca4573be92fd8eb2953
Sai Ram <sairam5074@gmail.com>
2021-06-18 22:38:33 +0530

    python flask 2

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from flask_restful import Api, Resource;
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "sairam": {
+        "Age": "24",
+    },
+    "jayasri": {
+        "Age": "22"
+    }
+}
 class Helloworld(Resource):
     def get(self, name):
-        return {'data': name}
+        return names[name]
     # def post(self):
     #     return {'data': 'Jayasri'}
     
